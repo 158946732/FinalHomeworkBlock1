4f5e0669826faf359cb1a5ed1ec98c9a4dd9b062
Alina <kalinka0611@mail.ru>
2023-01-24 23:59:34 +0300

    Написан код программы

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿int CountLess(string[] input, int n)
+{
+    int count = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+string[] GetNewArray(string[] array, int m)
+{
+    string[] newArray = new string[CountLess(array, m)];
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= m)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
+}
+
+string[] array1 = {"Hello", "2", "world", ":-)"};
+string[] array2 = GetNewArray(array1, 3);
+Console.WriteLine($"[{string.Join(", ", array1)}] -> [{string.Join(", ", array2)}]");
+
